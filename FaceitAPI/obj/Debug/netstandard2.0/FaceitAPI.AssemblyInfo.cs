291b955ed1b65def72c55ee461b742be9d760999
My perfect enemy <kacperf1234@gmail.com>
2019-07-12 16:26:57 +0200

    set.

--- FaceitAPI/obj/Debug/netstandard2.0/FaceitAPI.AssemblyInfo.cs
+++ FaceitAPI/obj/Debug/netstandard2.0/FaceitAPI.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FaceitAPI")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NoCompanyXD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Simple implementation of Faceit web api. ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaceitAPI")]
